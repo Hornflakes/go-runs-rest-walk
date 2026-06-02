b4ca89602a6a66c7ede83eb1574fc2e86522b84e
Hornflakes <cristianpetrugug@gmail.com>
2026-06-03 02:44:06 +0300

    feat(ts): minimal pairing server

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,27 +1,24 @@
-import { createServer } from 'http';
+import { createServer as createHttpServer } from 'http';
 import { WebSocketServer } from 'ws';
 import * as logger from './logger.js';
-import { createSocket } from './socket.js';
+import { createServer } from './server.js';
+import { Logger } from './logger.js';
 
-const server = createServer();
-const wss = new WebSocketServer({ server, path: '/' });
+const httpServer = createHttpServer();
+const wss = new WebSocketServer({ server: httpServer, path: '/' });
+
+const srv = createServer();
+
+srv.onPair = (pair) => {
+    const [s0, s1] = pair;
+    const log = new Logger(s0.playerId, s1.playerId);
+    log.milestone('websockets paired', '');
+};
 
 wss.on('connection', (ws, req) => {
-    const ip = req.socket.remoteAddress?.replace('::ffff:', '') ?? '';
-    const addr = ip + ':' + req.socket.remotePort;
-
-    const socket = createSocket(ws, addr);
-    logger.info('websocket connected', `addr=${addr}`);
-
-    socket.onMessage = (msg) => {
-        logger.info(
-            'echo',
-            `${logger.playerWithAddr(socket.playerId, addr)} msg=${JSON.stringify(msg)}`,
-        );
-        socket.send(msg);
-    };
+    srv.handleConnection(ws, req);
 });
 
-server.listen(37373, () => {
+httpServer.listen(37373, () => {
     logger.info('server listening', 'addr=:37373');
 });
