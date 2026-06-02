3c6a2e7898c318b7269b821db74966de28f5154e
Hornflakes <cristianpetrugug@gmail.com>
2026-06-03 02:30:28 +0300

    feat(ts): minimal echo socket

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,6 +1,7 @@
 import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
 import * as logger from './logger.js';
+import { createSocket } from './socket.js';
 
 const server = createServer();
 const wss = new WebSocketServer({ server, path: '/' });
@@ -8,7 +9,17 @@ const wss = new WebSocketServer({ server, path: '/' });
 wss.on('connection', (ws, req) => {
     const ip = req.socket.remoteAddress?.replace('::ffff:', '') ?? '';
     const addr = ip + ':' + req.socket.remotePort;
+
+    const socket = createSocket(ws, addr);
     logger.info('websocket connected', `addr=${addr}`);
+
+    socket.onMessage = (msg) => {
+        logger.info(
+            'echo',
+            `${logger.playerWithAddr(socket.playerId, addr)} msg=${JSON.stringify(msg)}`,
+        );
+        socket.send(msg);
+    };
 });
 
 server.listen(37373, () => {
